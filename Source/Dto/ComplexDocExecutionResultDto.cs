f43a4d0face8215db66c402a564fda85390948d8
Anthony Savchenko <anthony.savchenko@gmail.com>
2021-12-24 17:43:06 +1000

    Тип возвращаемого результата для составного документа

---
+++ Source/Dto/ComplexDocExecutionResultDto.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TK302FBPrinter.Dto
+{
+
+    public enum ComplexDocStatus
+    {
+        Success,
+        Error
+    }
+
+    public class ComplexDocExecutionResultDto
+    {
+        public ComplexDocExecutionResultDto(string errorDescription = null)
+        {
+            if (!string.IsNullOrEmpty(errorDescription))
+            {
+                ErrorDescription = errorDescription;
+                Status = ComplexDocStatus.Error;
+                return;
+            }
+            ErrorDescription = string.Empty;
+            Status = ComplexDocStatus.Success;
+        }
+
+        [Required]
+        public ComplexDocStatus Status { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string ErrorDescription { get; set; }
+    }
+}
