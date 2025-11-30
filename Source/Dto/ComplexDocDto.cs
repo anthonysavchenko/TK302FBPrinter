2d79503ee58794688c3c3c9df27284944c90bd32
Anthony Savchenko <anthony.savchenko@gmail.com>
2025-11-30 10:43:24 +0300

    Добавлены типы мест: Sector1 - Sector5

--- Source/Dto/ComplexDocDto.cs
+++ Source/Dto/ComplexDocDto.cs
@@ -15,7 +15,12 @@ namespace TK302FBPrinter.Dto
         Vip,
         Sofa,
         Simple,
-        BeatBox
+        BeatBox,
+        Sector1,
+        Sector2,
+        Sector3,
+        Sector4,
+        Sector5,
     }
 
     public enum ComplexDocPaymentTypeDto {
